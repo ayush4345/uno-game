2086f1e1b9f489ecb74a6587f6bc4204be4e1567
Ayush Kumar <sanusingh335@gmail.com>
2025-08-10 14:33:49 +0530

    changed url

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -5,27 +5,27 @@ export async function GET(request: NextRequest) {
   const manifest = {
     frame: {
       name: "Zunno",
-      homeUrl: "https://uno-game-pi.vercel.app",
-      iconUrl: "https://uno-game-pi.vercel.app/logo.jpg",
+      homeUrl: "https://zunno.xyz",
+      iconUrl: "https://zunno.xyz/logo.jpg",
       version: "1",
       subtitle: "leading game hub",
       description:
         "Zunno is a cutting-edge, multiplayer digital adaptation of the classic UNO game",
-      splashImageUrl: "https://uno-game-pi.vercel.app/logo.jpg",
+      splashImageUrl: "https://zunno.xyz/logo.jpg",
       primaryCategory: "games",
       splashBackgroundColor: "#0000a8",
       screenshotUrls: [
-        "https://uno-game-pi.vercel.app/images/screenshot_1.png",
-        "https://uno-game-pi.vercel.app/images/screenshot_2.png",
-        "https://uno-game-pi.vercel.app/images/screenshot_3.png",
+        "https://zunno.xyz/images/screenshot_1.png",
+        "https://zunno.xyz/images/screenshot_2.png",
+        "https://zunno.xyz/images/screenshot_3.png",
       ],
       tags: ["UNO", "mini-app", "base-app"],
-      heroImageUrl: "https://uno-game-pi.vercel.app/images/hero-1.png",
+      heroImageUrl: "https://zunno.xyz/images/hero-1.png",
       tagline: "UNO game on Blockchain",
       ogTitle: "Zunno",
       ogDescription:
         "Zunno is a cutting-edge, multiplayer digital adaptation of the classic UNO game",
-      ogImageUrl: "https://uno-game-pi.vercel.app/images/hero-1.png",
+      ogImageUrl: "https://zunno.xyz/images/hero-1.png",
     },
     accountAssociation: {
       header:
